b2b43a2a2f20c0230761a5bb4068ebca0fb4f4b3
lkreisel <laura.k0906@googlemail.com>
2025-12-04 22:20:46 +0100

    ++

--- code/lcs_prepare.R
+++ code/lcs_prepare.R
@@ -10,7 +10,7 @@ library(labelled)
 
 ############################ Data preparation ############################
 
-d <- haven::read_dta("data/cs/NSW_AFDC_CS.dta")
+d <- haven::read_dta("../data/cs/NSW_AFDC_CS.dta")
 d <- as.data.frame(zap_label(d))
 for (i in 1:ncol(d)) {
   if (is.numeric(d[, i])) {
@@ -80,7 +80,7 @@ stat.psid2 <- apply(lcs_psid2[which(lcs_psid2$treat == 0), X], 2, sd)
 out2 <- cbind.data.frame(stat.lcs.tr, stat.lcs.co, stat.psid, stat.psid2)
 out2[c("re75"), ] <- out2[c("re75"), ]/1000
 
-#count
+# count
 count.lcs.tr <- nrow(lcs[which(lcs$treat == 1), ])
 count.lcs.co <- nrow(lcs[which(lcs$treat == 0), ])
 count.psid <- nrow(lcs_psid[which(lcs_psid$treat == 0), ])
@@ -104,59 +104,5 @@ for (j in 1:n) {
 }
 rownames(sav) <- rep(rownames(out), each = 2)
 print(sav)
-
-table(lcs$treat)
-table(lcs_psid$treat)
-
-write.csv(sav, file = "tables/stats_lcs.csv", row.names = FALSE)
-
-############################ Trimming ############################
-
-Y <- "re79"
-treat <- "treat"
-
-# redefine covariates: removing "nchildren75" to be used as placebo outcome
-covar <- c("age", "educ", "nodegree", "married", "black", "hisp", "re75", "u75")
-
-## trimming
-p.forest <- probability_forest(X = lcs_psid.plus[, covar], 
-                               Y = as.factor(lcs_psid.plus$exp), seed = 1234, num.trees = 4000) # experimental indicator as treated
-lcs_psid.plus$ps <- p.forest$predictions[,2]
-range(lcs_psid.plus$ps)
-#hist(lcs_psid.plus$ps)
-
-# histogram of propensity scores
-round(quantile(lcs_psid.plus$ps[which(lcs_psid.plus$exp == 0 & lcs_psid.plus$ps > 0.6)], probs = seq(0, 1, 0.1)),3)
-threshold <- 0.9 # only 50 PSID control units have ps > 0.9
-nrow(subset(lcs_psid.plus, exp == 0 & ps > threshold)) # 27 control units
-nrow(subset(lcs_psid.plus, exp == 1 & ps > threshold))
-
-# trim experimental data
-lcs_trim_psid <- subset(lcs_psid.plus, exp == 1 & ps <= threshold) 
-lcs_trim_psid$ps <- NULL
-lcs_trim_psid$exp <- NULL
-lcs_trim_psid$psid1 <- NULL
-table(lcs_trim_psid$treat) # 773 units: 393 treated vs 380 controls; 1185-773 = 412 experimental units dropped
-head(lcs_trim_psid)
-summary(lm(re79 ~ treat, data = lcs_trim_psid))
-
-# propensity score matching without replacement
-set.seed(1234) # need to set seed b/c tie-breaking is random
-data <- subset(lcs_psid.plus, (treat == 1 | exp == 0) & ps <= threshold) # only use psid1 controls
-table(data$treat)
-data$ps <- probability_forest(X = data[, covar], 
-                              Y = as.factor(data$treat), seed = 1234, num.trees = 4000)$predictions[,2]
-mout <- Match(Y = data$re78, Tr = data$treat, X = data$ps, estimand = "ATT", M = 1,
-              BiasAdjust = FALSE, replace=FALSE, ties = FALSE)
-lcs_psid_trim <- data[c(mout$index.treated, mout$index.control), ]
-table(lcs_psid_trim$treat) # 1064 units: 532 treated vs 532 controls
-
-# estimate propensity score again
-lcs_psid_trim$ps_new <- probability_forest(X = lcs_psid_trim[, covar], 
-                                           Y = as.factor(lcs_psid_trim$treat), 
-                                           seed = 1234, num.trees = 4000)$predictions[,2]
-table(lcs_psid_trim$treat)
-
-save(lcs, lcs_psid, lcs_trim_psid, lcs_psid_trim, file = "data/lcs.RData")
-
+write.csv(sav, file = "../tables/stats_lcs.csv", row.names = FALSE)
 
---
+++ code/ldw_prepare.R
@@ -0,0 +1,119 @@
+# Application of the Outcome Weights Framework for Double Machine Learning to the Lalonde Study
+# Laura Kreisel
+# 2025
+
+## Original data from LaLonde
+# @imbens&xu (2024)
+
+library(haven)
+library(labelled)
+
+############################ Data preparation ############################
+
+process.data  <- function(filename) {
+  d <- haven::read_dta(filename)
+  d <- as.data.frame(zap_label(d))
+  for (i in 1:ncol(d)) {
+    if (is.numeric(d[, i])) {
+      d[, i] <- as.vector(d[, i])
+    }
+  }
+  if ("re74" %in% colnames(d)) {d$u74 <- ifelse(d$re74 == 0, 1, 0)}
+  d$u75 <- ifelse(d$re75 == 0, 1, 0)
+  return(d)
+}
+
+# experimental sample
+nsw <- process.data("../data/lalonde/nsw.dta")
+table(nsw$treat)
+nsw$sample <- NA
+nsw$sample[which(nsw$treat == 1)] <- 0
+nsw$sample[which(nsw$treat == 0)] <- 0.5
+table(nsw$treat, nsw$sample)
+nsw_tr <- subset(nsw, treat == 1)
+nsw_co <- subset(nsw, treat == 0)
+
+ldw <- process.data("../data/lalonde/nsw_dw.dta")
+ldw$sample <- NA
+ldw$sample[which(ldw$treat == 1)] <- 1
+ldw$sample[which(ldw$treat == 0)] <- 2
+table(ldw$treat, ldw$sample)
+ldw_tr <- ldw[which(ldw$treat == 1), ] # experimental treated 
+ldw_co <- ldw[which(ldw$treat == 0), ] # experimental control
+cps1 <- process.data("../data/lalonde/cps_controls.dta")
+psid1 <- process.data("../data/lalonde/psid_controls.dta")
+
+# nonexperimental samples
+cps1$sample <- 3
+ldw_cps <- rbind.data.frame(ldw_tr, cps1)
+table(ldw_cps$treat)
+
+psid1$sample <- 4
+ldw_psid <- rbind.data.frame(ldw_tr, psid1)
+table(ldw_psid$treat)
+
+cps1a <- subset(cps1, select =  -c(re74, u74))
+names(nsw)
+names(cps1a)
+nsw_cps <- rbind.data.frame(nsw_tr, cps1a)
+table(nsw_cps$treat)
+
+psid1a <- subset(psid1, select =  -c(re74, u74))
+nsw_psid <- rbind.data.frame(nsw_tr, psid1a)
+table(nsw_psid$treat)
+
+############################ Summary Statistics ############################
+
+X0 <- c("age", "education", "nodegree", "married", "black", "hispanic",  
+        "re75", "u75")
+
+X <- c("age", "education", "nodegree", "married", "black", "hispanic", 
+       "re75", "u75", "re74", "u74")
+
+# mean
+nsw.tr <- c(apply(nsw_tr[, X0], 2, mean), rep(NA, 2))
+nsw.co <- c(apply(nsw_co[, X0], 2, mean), rep(NA, 2))
+ldw.tr <- c(apply(ldw_tr[, X], 2, mean))
+ldw.co <- apply(ldw_co[, X], 2, mean)
+cps <- apply(cps1[, X], 2, mean)
+psid <- apply(psid1[, X], 2, mean)
+out <- cbind.data.frame(nsw.tr, nsw.co, cps, psid, ldw.tr, ldw.co)
+out[c("re75", "re74"), ] <- out[c("re75", "re74"), ]/1000
+
+# sd
+nsw.tr <- c(apply(nsw_tr[, X0], 2, sd), rep(NA, 2))
+nsw.co <- c(apply(nsw_co[, X0], 2, sd), rep(NA, 2))
+ldw.tr <- c(apply(ldw_tr[, X], 2, sd))
+ldw.co <- apply(ldw_co[, X], 2, sd)
+cps <- apply(cps1[, X], 2, sd)
+psid <- apply(psid1[, X], 2, sd)
+out2 <- cbind.data.frame(nsw.tr, nsw.co, cps, psid, ldw.tr, ldw.co)
+out2[c("re75", "re74"), ] <- out2[c("re75", "re74"), ]/1000
+
+# count
+count.nsw.tr <- nrow(nsw[which(nsw$treat == 1), ])
+count.nsw.co <- nrow(nsw[which(nsw$treat == 0), ])
+count.ldw.tr <- nrow(ldw[which(ldw$treat == 1), ])
+count.ldw.co <- nrow(ldw[which(ldw$treat == 0), ])
+count.cps     <- nrow(cps1)                       
+count.psid    <- nrow(psid1)                      
+out <- rbind(out, Counts = c(count.nsw.tr, count.nsw.co, count.cps, count.psid, count.ldw.tr, count.ldw.co))
+out2 <- rbind(out2, Counts = c(NA, NA, NA, NA, NA, NA))
+
+# columns are samples
+n <- nrow(out)   
+sav <- matrix("", n*2, ncol(out))
+for (j in 1:n) {
+  a <- out[j, ]
+  b <- out2[j, ]
+  if (rownames(out)[j] == "Counts") {
+    sav[j*2-1, ] <- sprintf("%.0f", a)
+    sav[j*2, ] <- rep("", ncol(out))  
+  } else {
+    sav[j*2-1, ] <- sprintf("%.2f", a)
+    sav[j*2, ] <- paste0("(", sprintf("%.2f",b), ")")
+  }
+}
+rownames(sav) <- rep(rownames(out), each = 2)
+print(sav)
+write.csv(sav, file = "../tables/stats_nsw_ldw.csv", row.names = FALSE)
